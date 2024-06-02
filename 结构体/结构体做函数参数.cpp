16574583998e46f01fe7ee301febb50fad5b530a
zhangyabo <3287308097@qq.com>
2024-06-02 21:36:00 +0800

    结构体做函数参数

---
+++ 结构体/结构体做函数参数.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+//struct Student {
+//	string name;
+//	int age;
+//	int score;
+//};
+//void printStu(struct Student student) {
+//	cout << "姓名是：" << student.name << " 年龄是：" << student.age << " 分数是：" << student.score << endl;
+//}
+//void printStu2(struct Student* student) {
+//	student->age = 77;
+//	cout << "姓名是：" << student->name << " 年龄是：" << student->age << " 分数是：" << student->score << endl;
+//}
+//int main() {
+//	Student student = { "李白",20,66 };
+//	//printStu(student);
+//	printStu2(&student);
+//	cout << "姓名是：" << student.name << " 年龄是：" << student.age << " 分数是：" << student.score << endl;
+//}
--- 结构体/结构体嵌套结构体.cpp
+++ 结构体/结构体嵌套结构体.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 
-struct Student {
-	string name;
-	int age;
-	int score;
-};
-struct Teacher {
-	int id;
-	string name;
-	int age;
-	struct Student stu;
-};
-int main() {
-	Teacher t;
-	t.id = 001;
-	t.name = "王";
-	t.age = 50;
-	t.stu.name = "赵刚";
-	t.stu.age = 22;
-	t.stu.score = 70;
-	cout << "老师姓名是：" << t.name << endl;
-}
+//struct Student {
+//	string name;
+//	int age;
+//	int score;
+//};
+//struct Teacher {
+//	int id;
+//	string name;
+//	int age;
+//	struct Student stu;
+//};
+//int main() {
+//	Teacher t;
+//	t.id = 001;
+//	t.name = "王";
+//	t.age = 50;
+//	t.stu.name = "赵刚";
+//	t.stu.age = 22;
+//	t.stu.score = 70;
+//	cout << "老师姓名是：" << t.name << endl;
+//}
