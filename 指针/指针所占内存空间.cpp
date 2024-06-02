2f2004185935c8bbb445ed51bc240a9a3ec3ef9c
zhangyabo <3287308097@qq.com>
2024-06-02 11:30:49 +0800

    指针

---
+++ 指针/指针所占内存空间.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+	32位操作系统中，占用4个字节
+	64位操作系统中，占用8个字节
+*/
+//int main() {
+//	int a = 10;
+//
+//	// 指针记录变量a的地址
+//	int* p = &a;
+//	cout << "int占用多大空间：" << sizeof(int *) << endl;
+//	cout << "char占用多大空间：" << sizeof(char *) << endl;
+//	cout << "float占用多大空间：" << sizeof(float *) << endl;
+//	cout << "double占用多大空间：" << sizeof(double *) << endl;
+//}
---
+++ 指针/指针的定义和使用.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+	数据类型* 指针变量名;
+*/
+
+
+//int main() {
+//	int a = 10;
+//	int* p;
+//	// 指针记录变量a的地址
+//	p = &a;
+//	cout << "a的地址是：" << &a << endl;
+//	cout << "指针p是：" << p << endl;
+//	// 指针前面加上 * 代表解引用，找到指针指向的内存中的数据
+//	*p = 20;
+//	cout << "改变之后a是" << a << endl;
+//}
