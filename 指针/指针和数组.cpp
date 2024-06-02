4d0a42ede5244870a3252976ae53799ddf1922fe
zhangyabo <3287308097@qq.com>
2024-06-02 16:10:50 +0800

    指针和数组

---
+++ 指针/指针和数组.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//int main() {
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int* p = arr; // arr就是数组的首地址
+//	//cout << arr << endl;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		cout << *p << endl;
+//		p++;
+//	}
+//}
--- 指针/空指针和野指针.cpp
+++ 指针/空指针和野指针.cpp
@@ -13,21 +13,21 @@ using namespace std;
 
 	0~255之间的内存编号是系统占用的，因此不可访问
 */
-int main() {
-	//// 空指针
-	//int* p = NULL;
-	////*p = 100; // 直接访问报错
-	////cout << *p << endl;
-
-	//int a = 10;
-	//p = &a; // 让p指向a
-	//*p = 100; // 不报错
-	//cout << *p << endl;
-	//cout << a << endl;
-
-
-	// 野指针
-	//int* p = (int *)0x1100;
-	//cout << *p << endl; // 访问报错
-
-}
+//int main() {
+//	// 空指针
+//	int* p = NULL;
+//	//*p = 100; // 直接访问报错
+//	//cout << *p << endl;
+//
+//	int a = 10;
+//	p = &a; // 让p指向a
+//	*p = 100; // 不报错
+//	cout << *p << endl;
+//	cout << a << endl;
+//
+//
+//	 //野指针
+//	int* p = (int *)0x1100;
+//	cout << *p << endl; // 访问报错
+//
+//}
