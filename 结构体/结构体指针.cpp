8751939c4dffd912ffa0d730bbc292bfc73a62fe
zhangyabo <3287308097@qq.com>
2024-06-02 21:20:53 +0800

    结构体嵌套结构体

--- 结构体/结构体指针.cpp
+++ 结构体/结构体指针.cpp
@@ -7,15 +7,15 @@ using namespace std;
 	利用操作符`->`可以通过结构体指针访问结构体属性
 */
 
-struct Student {
-	string name;
-	int age;
-	int score;
-};
-int main() {
-	Student student = { "玉环",17,90 };
-	struct Student* p = &student; // 通过指针指向结构体变量
-
-	// 访问
-	cout << p->name << endl;
-}
+//struct Student {
+//	string name;
+//	int age;
+//	int score;
+//};
+//int main() {
+//	Student student = { "玉环",17,90 };
+//	struct Student* p = &student; // 通过指针指向结构体变量
+//
+//	// 访问
+//	cout << p->name << endl;
+//}
