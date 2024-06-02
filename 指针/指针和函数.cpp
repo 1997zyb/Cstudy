bd5a24c7b9ab46123c21e98f68c9366704c28b6b
zhangyabo <3287308097@qq.com>
2024-06-02 16:23:33 +0800

    指针和函数

---
+++ 指针/指针和函数.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//void swap(int *p1,int *p2) {
+//	int temp = *p1;
+//	*p1 = *p2;
+//	*p2 = temp;
+//}
+//int main() {
+//	int a = 10;
+//	int b = 20;
+//	swap(&a, &b);
+//	cout << "a的值是：" << a << endl;
+//	cout << "b的值是：" << b << endl;
+//}
