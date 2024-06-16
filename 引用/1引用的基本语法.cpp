5bb472ad6ab364945505b4748ff494fa04194263
zhangyabo <3287308097@qq.com>
2024-06-16 11:09:57 +0800

    引用基本语法

---
+++ 引用/1引用的基本语法.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+	引用的基本语法：
+		数据类型 &别名 = 原名
+*/
+//int main() {
+//
+//	int a = 10;
+//	int& b = a;
+//	cout << "a等于：" << a << endl;
+//	cout << "b等于：" << b << endl;
+//
+//	b = 20;
+//	cout << "a等于：" << a << endl;
+//	cout << "b等于：" << b << endl;
+//}
