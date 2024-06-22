8aaaf85278dedef3e0d771c1a3be0fa67f8982b7
zhangyabo <3287308097@qq.com>
2024-06-22 17:28:10 +0800

    Update 封装学生类.cpp

--- 类和对象/封装学生类.cpp
+++ 类和对象/封装学生类.cpp
@@ -6,34 +6,34 @@ using namespace std;
 	属性还叫做 成员属性 或者成员变量
 	行为还叫做 成员行为 或者成员方法
 */
-class Student {
-public:
-	// 属性
-	string name;
-	int id; // 学号
-	// 行为
-	void showStu() {
-		cout << "姓名：" << name << "，学号是：" << id << endl;
-	}
-	// 给姓名赋值
-	void setName(string my_name) {
-		name = my_name;
-	}
-	// 给学号赋值
-	void setId(int my_id) {
-		id = my_id;
-	}
-};
-int main() {
-	Student s1;
-	s1.name = "张三";
-	s1.id = 1;
-	s1.setName("赵云");
-	s1.setId(7);
-	s1.showStu();
-
-	Student s2;
-	s2.name = "李四";
-	s2.id = 2;
-	s2.showStu();
-}
+//class Student {
+//public:
+//	// 属性
+//	string name;
+//	int id; // 学号
+//	// 行为
+//	void showStu() {
+//		cout << "姓名：" << name << "，学号是：" << id << endl;
+//	}
+//	// 给姓名赋值
+//	void setName(string my_name) {
+//		name = my_name;
+//	}
+//	// 给学号赋值
+//	void setId(int my_id) {
+//		id = my_id;
+//	}
+//};
+//int main() {
+//	Student s1;
+//	s1.name = "张三";
+//	s1.id = 1;
+//	s1.setName("赵云");
+//	s1.setId(7);
+//	s1.showStu();
+//
+//	Student s2;
+//	s2.name = "李四";
+//	s2.id = 2;
+//	s2.showStu();
+//}
