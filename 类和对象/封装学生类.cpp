c2204f81619c1f6cd537e6da5633259764393bc3
zhangyabo <3287308097@qq.com>
2024-06-22 17:26:44 +0800

    写一个学生类

---
+++ 类和对象/封装学生类.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+/*
+	类中的属性和行为，我们统一称为 成员
+	属性还叫做 成员属性 或者成员变量
+	行为还叫做 成员行为 或者成员方法
+*/
+class Student {
+public:
+	// 属性
+	string name;
+	int id; // 学号
+	// 行为
+	void showStu() {
+		cout << "姓名：" << name << "，学号是：" << id << endl;
+	}
+	// 给姓名赋值
+	void setName(string my_name) {
+		name = my_name;
+	}
+	// 给学号赋值
+	void setId(int my_id) {
+		id = my_id;
+	}
+};
+int main() {
+	Student s1;
+	s1.name = "张三";
+	s1.id = 1;
+	s1.setName("赵云");
+	s1.setId(7);
+	s1.showStu();
+
+	Student s2;
+	s2.name = "李四";
+	s2.id = 2;
+	s2.showStu();
+}
